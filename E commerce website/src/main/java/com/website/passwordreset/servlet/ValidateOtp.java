0c1fa3041eec20dd15c28be9be1999ca193e6e65
Dinakarasu <112040181+Dinakarasu@users.noreply.github.com>
2023-08-01 17:19:44 +0530

    Update ValidateOtp.java

--- E commerce website/src/main/java/com/website/passwordreset/servlet/ValidateOtp.java
+++ E commerce website/src/main/java/com/website/passwordreset/servlet/ValidateOtp.java
@@ -21,35 +21,26 @@ public class ValidateOtp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int value=Integer.parseInt(request.getParameter("otp"));
 		HttpSession session=request.getSession();
 		int otp=(int)session.getAttribute("otp");
 		
-		
-		
 		RequestDispatcher dispatcher=null;
 		
-		
-		if (value==otp) 
-		{
-			
+		response.setContentType("text/html;charset=UTF-8");
+		try (PrintWriter out=response.getWriter()){
+		if (value==otp) {
 				request.setAttribute("email", request.getParameter("email"));
 				request.setAttribute("status", "success");
 			  dispatcher=request.getRequestDispatcher("reset.jsp");
-			dispatcher.forward(request, response);
-			
+			dispatcher.forward(request, response);	
 		}
-		else
-		{
+		else{
+			out.print("Wrong otp try again!!! ----->     Click back button and then after refresh the enter otp page you get new OTP");
 			request.setAttribute("message","wrong otp");
-			
-		   dispatcher=request.getRequestDispatcher("otp.jsp");
-			dispatcher.forward(request, response);
-		
 		}
-		
+		}
 	}
 
 }
